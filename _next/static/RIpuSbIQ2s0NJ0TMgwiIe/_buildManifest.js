263006b25265b7e149fd8988147cdd7a1f24de0f
ansaryergesh <yergeshovansar@gmail.com>
2021-05-28 16:15:03 +0600

    updated

---
+++ _next/static/RIpuSbIQ2s0NJ0TMgwiIe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/505-0ecc6d61304948a10f1c.js","static/chunks/pages/index-0a9e1d4951d85396d371.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
